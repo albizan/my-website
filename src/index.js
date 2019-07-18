a3509501b8c3da7ef4c737155e25e655d129c4cf
Alberto Zanotti <albizan@hotmail.it>
2019-07-19 00:36:02 +0200

    Import new ThemeState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { MyThemeProvider, } from "./ThemeContext";
+import App from './App.jsx';
+import ThemeState from "./context/theme/state";
 
 ReactDOM.render(
-  <MyThemeProvider>
+  <ThemeState>
     <App />
-  </MyThemeProvider>
+  </ThemeState>
   , document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
