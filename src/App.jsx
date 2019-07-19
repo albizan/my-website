c2edc81eaad278d064d9fe67f5aa99509a4fa7b6
Alberto Zanotti <albizan@hotmail.it>
2019-07-19 23:00:00 +0200

    Created Themed Wrapper for basic theming styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useThemes } from './context/theme/context';
-import styled, { withTheme } from 'styled-components';
-import { backgroundColor, textColor, buttonBackgroundColor, buttonTextColor } from './theme';
+import React from 'react'
+import styled, { withTheme } from 'styled-components'
 
-function App(props) {
+/* Custom */
+import Landing from './components/Landing'
+import { backgroundColor, textColor } from './theme'
 
-  const { toggle } = useThemes();
-
-  const Button = styled.button`
-    background: ${buttonBackgroundColor};
-    border: none;
-    border-radius: 0.3em;
-    box-shadow: none;
-    color: ${buttonTextColor};
-    cursor: pointer;
-    font-size: 1em;
-    padding: 0.5em 1em;
-  `;
-
-  const Wrapper = styled.div`
+function App() {
+  const ThemedApp = styled.div`
     background-color: ${backgroundColor};
     color: ${textColor};
-  `;
+  `
 
   return (
-    <Wrapper>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <Button onClick={() => toggle()}>
-            {props.theme.mode === 'dark' ? "Switch to Light Mode" : "Switch to Dark Mode"}
-          </Button>
-        </p>
-      </header>
-    </Wrapper>
-  );
+    <ThemedApp>
+      <Landing />
+    </ThemedApp>
+  )
 }
 
-/* Export HOC */
-export default withTheme(App);
+/* Export HOC, use styled-theming */
+export default withTheme(App)
