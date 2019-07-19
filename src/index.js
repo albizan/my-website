b4630f05dcb86afea22127ca3c0cf305e309c46c
Alberto Zanotti <albizan@hotmail.it>
2019-07-19 22:58:32 +0200

    Deleted unused imports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App.jsx';
-import ThemeState from "./context/theme/state";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './assets/css/index.css'
+import App from './App.jsx'
+import ThemeState from './context/theme/state'
 
 ReactDOM.render(
   <ThemeState>
     <App />
-  </ThemeState>
-  , document.getElementById('root'));
+  </ThemeState>,
+  document.getElementById('root')
+)
