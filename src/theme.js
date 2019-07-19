0d0d969d56b73f4f190827a96eb9790f654f3214
Alberto Zanotti <albizan@hotmail.it>
2019-07-19 22:59:12 +0200

    Defined theme styles for light/dark theme

--- src/theme.js
+++ src/theme.js
@@ -1,21 +1,11 @@
-import theme from 'styled-theming';
+import theme from 'styled-theming'
 
 export const backgroundColor = theme('mode', {
   light: '#fafafa',
-  dark: '#222'
-});
+  dark: '#222',
+})
 
 export const textColor = theme('mode', {
   light: '#000',
-  dark: '#fff'
-});
-
-export const buttonBackgroundColor = theme('mode', {
-  light: '#222',
-  dark: '#eee'
-});
-
-export const buttonTextColor = theme('mode', {
-  light: '#eee',
-  dark: '#222'
-});
+  dark: '#fff',
+})
