0efa6c6a22fb934f0472e69d10a56b7e0f12524f
Alberto Zanotti <albizan@hotmail.it>
2019-07-20 15:57:46 +0200

    Style main wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import Landing from './components/Landing'
 import { backgroundColor, textColor } from './theme'
 
 function App() {
+  /* Define The Wrapper of the App, used to theme generic backgrounds and text color */
   const ThemedApp = styled.div`
+    font-family: 'Quicksand';
     background-color: ${backgroundColor};
     color: ${textColor};
   `
