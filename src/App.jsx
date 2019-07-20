d7cdcab68738f539d44e024dde6813704c878a8c
Alberto Zanotti <albizan@hotmail.it>
2019-07-20 23:37:12 +0200

    Add ThemeToggle

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import styled, { withTheme } from 'styled-components'
 
 /* Custom */
 import Landing from './components/Landing'
+import ThemeToggle from './components/ThemeToggle'
 import { backgroundColor, textColor } from './theme'
 
 function App() {
   /* Define The Wrapper of the App, used to theme generic backgrounds and text color */
   const ThemedApp = styled.div`
     font-family: 'Quicksand';
-    background-color: ${backgroundColor};
+    background-color: ${backgroundColorDarker};
     color: ${textColor};
   `
 
   return (
     <ThemedApp>
+      <ThemeToggle />
       <Landing />
     </ThemedApp>
   )
