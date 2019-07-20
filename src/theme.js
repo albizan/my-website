7ae01c6ae225c65468aef841a766d3c34ede101a
Alberto Zanotti <albizan@hotmail.it>
2019-07-20 15:57:36 +0200

    Adjust Background and text colors for both themes

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,11 @@
 import theme from 'styled-theming'
 
 export const backgroundColor = theme('mode', {
-  light: '#fafafa',
-  dark: '#222',
+  light: '#F7F7F7',
+  dark: '#202123',
 })
 
 export const textColor = theme('mode', {
-  light: '#000',
-  dark: '#fff',
+  light: '#202123',
+  dark: '#F7F7F7',
 })
