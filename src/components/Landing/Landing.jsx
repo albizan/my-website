6508dda850f78028c1619f25f2cba4443ab6603e
Alberto Zanotti <albizan@hotmail.it>
2019-07-21 17:42:41 +0200

    Add Typing animation on text

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled, { withTheme } from 'styled-components'
+import Typist from 'react-typist'
+
+/* Custom Imports */
 import { backgroundColorDarker, textColor } from '../../theme'
 
 const Landing = () => {
@@ -15,8 +18,27 @@ const Landing = () => {
   return (
     <ThemedWrapper>
       <div className="h-screen flex flex-col justify-center items-center">
-        <h1 className="text-4xl">Hello World!</h1>
-        <h2 className="text-2xl">My name is Alberto Zanotti</h2>
+        <p className="text-xl sm:text-3xl">Hello World!</p>
+        <Typist
+          cursor={{
+            hideWhenDone: true,
+            hideWhenDoneDelay: 250,
+          }}
+          className="text-sm sm:text-xl text-center"
+        >
+          <Typist.Delay ms={750} />
+          My name is Alberto Zanotti
+          <Typist.Delay ms={500} />
+          <br />
+          <span>I am a junior web developer</span>
+          <Typist.Delay ms={500} />
+          <br />
+          <span>I love new web technologies</span>
+          <Typist.Delay ms={500} />
+          <span> such as React, Tailwindcss and PHP</span>
+          <Typist.Backspace count={3} delay={100} />
+          <span>Node.js</span>
+        </Typist>
       </div>
     </ThemedWrapper>
   )
