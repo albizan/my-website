3e34b7dd6d56862e4d711e2bfaf75cfde99c4428
Alberto Zanotti <albizan@hotmail.it>
2019-07-21 17:06:43 +0200

    Fixed proper color hierarchy

--- src/theme.js
+++ src/theme.js
@@ -1,26 +1,26 @@
 import theme from 'styled-theming'
 
 export const backgroundColorDarker = theme('mode', {
-  light: '##efefef',
-  dark: '#4f4f4f',
+  light: '#efefef',
+  dark: '#202123',
 })
 
 export const backgroundColorLighter = theme('mode', {
-  light: '#F7F7F7',
+  light: '#efefef',
   dark: '#202123',
 })
 
 export const textColor = theme('mode', {
   light: '#202123',
-  dark: '#F7F7F7',
+  dark: '#efefef',
 })
 
 export const toggleBackgroundColor = theme('mode', {
   light: '#202123',
-  dark: '#F7F7F7',
+  dark: '#efefef',
 })
 
 export const toggleTextColor = theme('mode', {
-  light: '#F7F7F7',
+  light: '#efefef',
   dark: '#202123',
 })
