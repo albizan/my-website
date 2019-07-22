bfc64b554a8b1b11f11ebaf98ea7012e4ef24028
Alberto Zanotti <albizan@hotmail.it>
2019-07-22 12:23:24 +0200

    Improve Comments

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './assets/css/index.css'
+
+/* Custom imports */
 import App from './App.jsx'
+
+/* Import ThemeState component from my contexts */
 import ThemeState from './context/theme/state'
 
+/* Import css styles, generated with postcss and tailwind */
+import './assets/css/index.css'
+
+/*
+  ThemeState wraps App so that App sub-components can get theme mode
+  and toggle function to change theme mode
+ */
 ReactDOM.render(
   <ThemeState>
     <App />
