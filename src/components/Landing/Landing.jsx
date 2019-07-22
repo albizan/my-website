03976fc425a2436948f3bdbc61e7f22357496d2b
Alberto Zanotti <albizan@hotmail.it>
2019-07-22 21:04:06 +0200

    Adjusted text size on mobile

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled, { withTheme } from 'styled-components'
-import Typist from 'react-typist'
 
 /* Custom Imports */
 import { backgroundColorDarker, textColor } from '../../theme'
@@ -10,37 +9,19 @@ const Landing = () => {
     Define wrapper for current component
     Used to theme component with styledComponents and styledTheming
   */
-  const ThemedWrapper = styled.div`
+  const LandingStyleWrapper = styled.div`
     background-color: ${backgroundColorDarker};
     color: ${textColor};
   `
 
   return (
-    <ThemedWrapper>
-      <div className="h-screen flex flex-col justify-center items-center">
-        <p className="text-xl sm:text-3xl">Hello World!</p>
-        <Typist
-          cursor={{
-            hideWhenDone: true,
-            hideWhenDoneDelay: 250,
-          }}
-          className="text-sm sm:text-xl text-center"
-        >
-          <Typist.Delay ms={750} />
-          My name is Alberto Zanotti
-          <Typist.Delay ms={500} />
-          <br />
-          <span>I am a junior web developer</span>
-          <Typist.Delay ms={500} />
-          <br />
-          <span>I love new web technologies</span>
-          <Typist.Delay ms={500} />
-          <span> such as React, Tailwindcss and PHP</span>
-          <Typist.Backspace count={3} delay={100} />
-          <span>Node.js</span>
-        </Typist>
+    <LandingStyleWrapper>
+      <div className="h-screen flex justify-center items-center">
+        <p className="text-2xl sm:text-4xl text-center font-bold">
+          Hi There! My name's Alberto
+        </p>
       </div>
-    </ThemedWrapper>
+    </LandingStyleWrapper>
   )
 }
 
