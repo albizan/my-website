9c20ccb43428c3ee4c5f939733c02738c6bf2347
Alberto Zanotti <albizan@hotmail.it>
2019-07-24 12:23:46 +0200

    Define colors for landing

---
+++ src/themes/landing.js
@@ -0,0 +1,11 @@
+import theme from 'styled-theming'
+
+/* Landing Colors */
+export const landingBackgroundLighter = theme('mode', {
+  light: '#c169d7',
+  dark: '#485563',
+})
+export const landingBackgroundDarker = theme('mode', {
+  light: '#8a4fc7',
+  dark: '#29323c',
+})
