cd9cbf9cd35a19179b43c0904b0cdf6e8194980c
Alberto Zanotti <albizan@hotmail.it>
2019-07-24 19:24:17 +0200

    Created Greetings Component

---
+++ src/components/Greetings/Greetings.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Greetings = () => {
+  return (
+    <section className="my-32 text-center">
+      <div className="mx-auto px-8 md:w-3/5">
+        <h3 className="text-xl sm:text-4xl font-bold">Hi, nice to meet you!</h3>
+        <p className="mt-8 sm:text-lg">
+          Welcome on my website, I am glad you visited my personal space, here
+          is where I am proud to show my skills, projects, past experiences and
+          future hopes in the web development field.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Greetings
