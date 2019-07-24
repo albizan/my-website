b1021015a59893dfe6a1644127608c581072d0de
Alberto Zanotti <albizan@hotmail.it>
2019-07-24 16:49:50 +0200

    Created base theme colors

---
+++ src/themes/base.js
@@ -0,0 +1,12 @@
+import theme from 'styled-theming'
+
+/* Landing Colors */
+export const baseBackgroundColor = theme('mode', {
+  light: '#c169d7',
+  dark: '#393d46',
+})
+
+export const baseTextColor = theme('mode', {
+  light: '#393d46',
+  dark: '#efefef',
+})
