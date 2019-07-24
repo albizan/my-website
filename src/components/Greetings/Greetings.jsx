63dbbc444a997295ce9dd395aef1c878d080d14c
Alberto Zanotti <albizan@hotmail.it>
2019-07-24 21:47:14 +0200

    Fixed font size

--- src/components/Greetings/Greetings.jsx
+++ src/components/Greetings/Greetings.jsx
@@ -4,8 +4,10 @@ const Greetings = () => {
   return (
     <section className="my-32 text-center">
       <div className="mx-auto px-8 md:w-3/5">
-        <h3 className="text-xl sm:text-4xl font-bold">Hi, nice to meet you!</h3>
-        <p className="mt-8 sm:text-lg">
+        <h3 className="text-2xl sm:text-3xl font-bold">
+          Hi, nice to meet you!
+        </h3>
+        <p className="mt-8 text-lg sm:text-xl font-semibold">
           Welcome on my website, I am glad you visited my personal space, here
           is where I am proud to show my skills, projects, past experiences and
           future hopes in the web development field.
