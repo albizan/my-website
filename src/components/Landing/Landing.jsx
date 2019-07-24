4d86be8334be68d8aa3cfa2be4e8f51e6dbee41d
Alberto Zanotti <albizan@hotmail.it>
2019-07-24 12:24:12 +0200

    Use new colors scheme

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import styled, { withTheme } from 'styled-components'
 
 /* Custom Imports */
-import { backgroundColorDarker, textColor } from '../../theme'
+import {
+  landingBackgroundLighter,
+  landingBackgroundDarker,
+} from '../../themes/landing'
 
 const Landing = () => {
   /* 
@@ -10,18 +13,24 @@ const Landing = () => {
     Used to theme component with styledComponents and styledTheming
   */
   const LandingStyleWrapper = styled.div`
-    background-color: ${backgroundColorDarker};
-    color: ${textColor};
+    background-color: ${landingBackgroundDarker};
+    background-image: linear-gradient(
+      45deg,
+      ${landingBackgroundLighter} 0%,
+      ${landingBackgroundDarker} 100%
+    );
   `
 
   return (
     <LandingStyleWrapper>
       <div className="h-screen flex justify-center items-center">
-        <div className="mx-4 tracking-wide text-center ">
-          <p className="text-xl sm:text-3xl font-bold">
+        <div className="mx-2 tracking-wide text-center text-white">
+          <p className="text-2xl sm:text-3xl font-bold">
             Hello There! My name's Alberto
           </p>
-          <p className="sm:text-xl font-thin">I am a junior web developer</p>
+          <p className="text-sm sm:text-xl font-thin">
+            I am a junior web developer
+          </p>
         </div>
       </div>
     </LandingStyleWrapper>
