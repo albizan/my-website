1028ec288703f087972ff6386ee528afa33ec3a2
Alberto Zanotti <albizan@hotmail.it>
2019-07-24 19:23:49 +0200

    Defined accent colors

--- src/themes/base.js
+++ src/themes/base.js
@@ -2,11 +2,21 @@ import theme from 'styled-theming'
 
 /* Landing Colors */
 export const baseBackgroundColor = theme('mode', {
-  light: '#c169d7',
+  light: '#fdfdfd',
   dark: '#393d46',
 })
 
 export const baseTextColor = theme('mode', {
   light: '#393d46',
-  dark: '#efefef',
+  dark: '#fdfdfd',
+})
+
+export const baseTextColorInverted = theme('mode', {
+  light: '#fdfdfd',
+  dark: '#393d46',
+})
+
+export const accentColor = theme('mode', {
+  light: '#8a4fc7',
+  dark: '#ec625f',
 })
