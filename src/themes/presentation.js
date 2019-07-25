7cf67bfbcf7b5586e2da11b499e6e748be14606f
Alberto Zanotti <albizan@hotmail.it>
2019-07-25 19:15:49 +0200

    Added presentation theme config

---
+++ src/themes/presentation.js
@@ -0,0 +1,16 @@
+import theme from 'styled-theming'
+
+export const textColor = theme('mode', {
+  light: '#efefef',
+  dark: '#efefef',
+})
+
+/* Background colors */
+export const backgroundLighter = theme('mode', {
+  light: '#ff7675',
+  dark: '#e056fd',
+})
+export const backgroundDarker = theme('mode', {
+  light: '#6c5ce7',
+  dark: '#f50045',
+})
