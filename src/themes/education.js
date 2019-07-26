fa2ed592ab4d21a324a9229295b8b9977dcf6e7e
Alberto Zanotti <albizan@hotmail.it>
2019-07-26 22:42:36 +0200

    Created Education theme file

---
+++ src/themes/education.js
@@ -0,0 +1,16 @@
+import theme from 'styled-theming'
+
+export const textColor = theme('mode', {
+  light: '#393d46',
+  dark: '#efefef',
+})
+
+/* Background colors */
+export const backgroundLighter = theme('mode', {
+  light: '#fdfdfd',
+  dark: '#f5576c',
+})
+export const backgroundDarker = theme('mode', {
+  light: '#fdfdfd',
+  dark: '#6d5ce8',
+})
