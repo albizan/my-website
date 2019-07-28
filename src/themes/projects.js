0c8894a6779f11d386f3cd989eacedf6a43e2be3
Alberto Zanotti <albizan@hotmail.it>
2019-07-28 22:25:07 +0200

    create projects theme variables

---
+++ src/themes/projects.js
@@ -0,0 +1,16 @@
+import theme from 'styled-theming'
+
+export const textColor = theme('mode', {
+  light: '#efefef',
+  dark: '#efefef',
+})
+
+/* Background colors */
+export const backgroundLighter = theme('mode', {
+  light: '#77B4D8',
+  dark: '#111111',
+})
+export const backgroundDarker = theme('mode', {
+  light: '#6e45e2',
+  dark: '#111111',
+})
