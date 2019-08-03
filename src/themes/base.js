05ff607daefc02846cc065465ac9c942833e2397
Alberto Zanotti <albizan@hotmail.it>
2019-08-03 18:33:40 +0200

    Added divider Color

--- src/themes/base.js
+++ src/themes/base.js
@@ -1,21 +1,21 @@
 import theme from 'styled-theming'
 
-export const baseBackgroundColor = theme('mode', {
+export const backgroundColor = theme('mode', {
   light: '#fdfdfd',
   dark: '#000',
 })
 
-export const baseTextColor = theme('mode', {
+export const textColor = theme('mode', {
   light: '#333',
   dark: '#fdfdfd',
 })
 
-export const baseTextColorInverted = theme('mode', {
-  light: '#fdfdfd',
-  dark: '#393d46',
-})
-
-export const titleColor = theme('mode', {
+export const accentColor = theme('mode', {
   light: '#5a67d8',
   dark: '#5a67d8',
+})
+
+export const dividerColor = theme('mode', {
+  light: '#718096',
+  dark: '#718096',
 })
