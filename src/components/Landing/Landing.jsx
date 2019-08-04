ace4b0dd2d92698628def9f33f5c72eb41d05604
Alberto Zanotti <albizan@hotmail.it>
2019-08-04 21:50:18 +0200

    Added background image

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,6 +3,7 @@ import styled, { withTheme } from 'styled-components'
 
 /* Custom Imports */
 import avatar from '../../assets/img/avatar.png'
+import bg from '../../assets/img/bg.jpg'
 import {
   landingTextColor,
   landingBackgroundLighter,
@@ -16,18 +17,31 @@ const Landing = () => {
   */
   const LandingStyleWrapper = styled.div`
     color: ${landingTextColor};
+    background: url(${bg}) no-repeat center center fixed;
+    background-size: cover;
+    /* background-color: ${landingBackgroundDarker};
+    background-image: linear-gradient(
+      40deg,
+      ${landingBackgroundLighter} 0%,
+      ${landingBackgroundDarker} 90%
+    ); */
+  `
+  const Overlay = styled.div`
     background-color: ${landingBackgroundDarker};
     background-image: linear-gradient(
       40deg,
       ${landingBackgroundLighter} 0%,
       ${landingBackgroundDarker} 90%
     );
+    opacity: 0.7;
+    z-index: 0;
   `
 
   return (
     <LandingStyleWrapper>
-      <div className="h-screen flex flex-col justify-center items-center select-none">
-        <div className="tracking-wide text-center px-3 mt-24">
+      <div className="h-screen flex flex-col justify-center items-center select-none relative">
+        <Overlay className="absolute inset-0" />
+        <div className="tracking-wide text-center px-3 mt-24 z-10">
           <p className="text-4xl md:text-5xl font-bold">
             Hello There! My name's Alberto
           </p>
@@ -35,7 +49,7 @@ const Landing = () => {
             I am a junior web developer
           </p>
         </div>
-        <div className="w-32 h-32 sm:w-48 sm:w-48 mt-8">
+        <div className="w-32 h-32 sm:w-48 sm:w-48 mt-8 z-10">
           <img className="rounded-full" src={avatar} alt="my avatar" />
         </div>
       </div>
