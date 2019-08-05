a30d1b0eff78eeeed34f7143e2437973f9a0c705
Alberto Zanotti <albizan@hotmail.it>
2019-08-05 10:37:08 +0200

    Import Skills, changed width for timeline

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import styled, { withTheme } from 'styled-components'
 
 /* Custom Imports */
 import Timeline from './TimeLine'
+import Skills from './Skills'
 
 const Resume = () => {
   return (
     <section className="container section-container">
       <h3 className="section-title">Resume</h3>
       <div className="block md:flex">
-        <div className="w-full md:w-2/3">
+        <div className="w-full md:w-1/3">
           <h4 className="inline-block text-xl sm:text-2xl font-bold border-b-2 border-indigo-600 mb-8">
             Education
           </h4>
           <Timeline />
         </div>
-        <div className="w-full md:w-1/3" />
+        <div className="w-full md:w-2/3" />
       </div>
     </section>
   )
 }
 
-export default withTheme(Resume)
+export default Resume
