137bd3ce783a3dbf6adead1ca704e031f6da90d1
Alberto Zanotti <albizan@hotmail.it>
2019-08-05 10:41:30 +0200

    Fixed width

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -9,13 +9,18 @@ const Resume = () => {
     <section className="container section-container">
       <h3 className="section-title">Resume</h3>
       <div className="block md:flex">
-        <div className="w-full md:w-1/3">
+        <div className="w-full md:w-2/5 mb-16 md:mr-16">
           <h4 className="inline-block text-xl sm:text-2xl font-bold border-b-2 border-indigo-600 mb-8">
             Education
           </h4>
           <Timeline />
         </div>
-        <div className="w-full md:w-2/3" />
+        <div className="w-full md:w-3/5">
+          <h4 className="inline-block text-xl sm:text-2xl font-bold border-b-2 border-indigo-600 mb-8">
+            Coding Skills
+          </h4>
+          <Skills />
+        </div>
       </div>
     </section>
   )
