feb094563484ee66cdd289700952e89bbf66df2f
Alberto Zanotti <albizan@hotmail.it>
2019-08-05 22:33:03 +0200

    Decreased title font size

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -10,13 +10,13 @@ const Resume = () => {
       <h3 className="section-title">Resume</h3>
       <div className="block md:flex">
         <div className="w-full md:w-1/2 mb-16 md:mr-16">
-          <h4 className="inline-block text-xl sm:text-2xl font-bold border-b-2 border-indigo-600 mb-8">
+          <h4 className="inline-block text-lg sm:text-xl font-semibold border-b-2 border-indigo-600 mb-8">
             Education
           </h4>
           <Timeline />
         </div>
         <div className="w-full md:w-1/2">
-          <h4 className="inline-block text-xl sm:text-2xl font-bold border-b-2 border-indigo-600 mb-8">
+          <h4 className="inline-block text-lg sm:text-xl font-semibold border-b-2 border-indigo-600 mb-8">
             Coding Skills
           </h4>
           <Skills />
