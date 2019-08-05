311f9f89ce0c45d0195d11d99ddb295f7a0458c9
Alberto Zanotti <albizan@hotmail.it>
2019-08-05 22:52:25 +0200

    Created first skillbar

--- src/components/Resume/Skills/Skills.jsx
+++ src/components/Resume/Skills/Skills.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import styled, { withTheme } from 'styled-components'
+
+const SkillBarWrapper = styled.div``
+const SkillBar = styled.div`
+  width: ${props => props.width || '50%'};
+`
 
 const Skills = () => {
-  return <p />
+  return (
+    <div>
+      <div className="flex">
+        <div className="w-1/2">
+          <h5>HTML</h5>
+        </div>
+        <div className="w-1/2 text-right text-xs">
+          <p>70%</p>
+        </div>
+      </div>
+      <div className="w-full p-1 bg-transparent border-2 rounded border-indigo-600">
+        <SkillBar
+          width="70%"
+          className="rounded h-1 bg-indigo-600 text-right px-2 py-1"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Skills
