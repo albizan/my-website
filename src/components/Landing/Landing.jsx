8e4e8832dc0048af479e7594bdb15bb921ba8b00
Alberto Zanotti <albizan@hotmail.it>
2019-08-07 11:30:02 +0200

    Use styled components for h1 and h2

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -4,11 +4,7 @@ import styled, { withTheme } from 'styled-components'
 /* Custom Imports */
 import avatar from '../../assets/img/avatar.png'
 import bg from '../../assets/img/bg.jpg'
-import {
-  landingTextColor,
-  landingBackgroundLighter,
-  landingBackgroundDarker,
-} from '../../themes/landing'
+import { titleColor, overlayLighter, overlayDarker } from '../../themes/landing'
 
 const Landing = () => {
   /* 
@@ -16,19 +12,24 @@ const Landing = () => {
     Used to theme component with styledComponents and styledTheming
   */
   const LandingStyleWrapper = styled.div`
-    color: ${landingTextColor};
     background: url(${bg}) no-repeat center center fixed;
     background-size: cover;
   `
   const Overlay = styled.div`
-    background-color: ${landingBackgroundDarker};
+    background-color: ${overlayDarker};
     background-image: linear-gradient(
       40deg,
-      ${landingBackgroundLighter} 0%,
-      ${landingBackgroundDarker} 90%
+      ${overlayLighter} 0%,
+      ${overlayDarker} 90%
     );
     opacity: 0.6;
     z-index: 0;
+  `
+  const Title = styled.h1`
+    color: ${titleColor};
+  `
+  const Subtitle = styled.h2`
+    color: ${titleColor};
   `
 
   return (
@@ -36,8 +37,10 @@ const Landing = () => {
       <div className="h-screen flex flex-col justify-center items-center select-none relative">
         <Overlay className="absolute inset-0" />
         <div className="tracking-wider text-center font-semibold px-3 mt-20 z-10">
-          <h1 className="text-3xl md:text-4xl">Alberto Zanotti</h1>
-          <h2 className="text-lg md:text-xl">Junior web developer</h2>
+          <Title className="text-3xl md:text-4xl">Alberto Zanotti</Title>
+          <Subtitle className="text-lg md:text-xl">
+            Junior web developer
+          </Subtitle>
         </div>
         <div className="w-24 h-24 sm:w-40 sm:w-40 mt-6 z-10">
           <img className="rounded-full" src={avatar} alt="my avatar" />
