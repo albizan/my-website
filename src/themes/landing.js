b384b25c4b78ff7b830d5c6f6a9304009046265d
Alberto Zanotti <albizan@hotmail.it>
2019-08-07 11:29:41 +0200

    Improve comments

--- src/themes/landing.js
+++ src/themes/landing.js
@@ -1,17 +1,23 @@
 import theme from 'styled-theming'
 
-/* Landing Background Colors */
-export const landingBackgroundLighter = theme('mode', {
+/*
+  Landing Overlay Colors
+  On light theme, a gradient is used. This gradients overlays the image background of the Landing Section
+  On dark theme, a dark overlay without gradient is applied. This is used to darken the background image
+*/
+export const overlayLighter = theme('mode', {
   light: '#FC5C7D',
   dark: '#111111',
 })
-export const landingBackgroundDarker = theme('mode', {
+export const overlayDarker = theme('mode', {
   light: '#5a67d8',
   dark: '#111111',
 })
 
-/* Landing Text Color */
-export const landingTextColor = theme('mode', {
+/*
+  These are the color used for Landing Page titles
+*/
+export const titleColor = theme('mode', {
   light: '#fff',
   dark: '#efefef',
 })
