6c6991309c24811198c6f7095a3f18bfa3d7353d
Alberto Zanotti <albizan@hotmail.it>
2019-08-07 12:39:27 +0200

    Added more skills

--- src/components/Resume/Skills/Skills.jsx
+++ src/components/Resume/Skills/Skills.jsx
@@ -3,8 +3,14 @@ import Skill from './Skill'
 
 const Skills = () => {
   return (
-    <div>
+    <div id="Skills Wrapper">
       <Skill skill="HTML" value="70%" />
+      <Skill skill="CSS" value="60%" />
+      <Skill skill="Javascript - Browser" value="60%" />
+      <Skill skill="Javascript - Server" value="70%" />
+      <Skill skill="Typescript" value="70%" />
+      <Skill skill="Reactjs" value="65%" />
+      <Skill skill="Nodejs" value="75%" />
     </div>
   )
 }
