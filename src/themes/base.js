791a7c89a96ed43334871012a99ca9afdd8ce3f0
Alberto Zanotti <albizan@hotmail.it>
2019-08-07 11:29:31 +0200

    Improve comments

--- src/themes/base.js
+++ src/themes/base.js
@@ -1,22 +1,46 @@
 import theme from 'styled-theming'
 
+/* 
+  Define generic background color for both themes
+  @light is a white background
+  @dark is a dark-blueish-grey background
+  Other background colors may be used in specific Section Theme files
+*/
 export const backgroundColor = theme('mode', {
   light: '#fff',
   dark: '#000',
 })
 
+/* 
+  Define generic text color for both themes
+  @light is a dark gray color
+  @dark is a very light grey color
+  Other text colors may be used in specific Section Theme files
+*/
 export const textColor = theme('mode', {
   light: '#333',
   dark: '#fdfdfd',
 })
+
+/* 
+  Define darker text color, this may be applied to Section Titles to define hierarchy in components
+  @light is a very dark gray color
+  @dark is 100% white
+*/
 export const textColorDarker = theme('mode', {
   light: '#222',
   dark: '#fff',
 })
 
+/* 
+  Define the main accent color for both themes
+  @light is an indigo (blue-violet) color called 'Exodus Fruit'
+  @dark is a special tint of red, called 'Watermelon'
+  Other text colors or gradients may be used in specific Section Theme files
+*/
 export const accentColor = theme('mode', {
-  light: '#5a67d8',
-  dark: '#5a67d8',
+  light: '#6c5ce7',
+  dark: '#ff4757',
 })
 
 export const dividerColor = theme('mode', {
