dbe8fc691e6ef8f9c83209f1f47b3a39ca21b78f
Alberto Zanotti <albizan@hotmail.it>
2019-08-09 17:19:10 +0200

    Rewrite theme toggle

--- src/components/ThemeToggle/ThemeToggle.jsx
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled, { withTheme } from 'styled-components'
+import { FiSun, FiMoon } from 'react-icons/fi'
 
 /* Custom */
 import { useThemes } from '../../context/theme/context'
@@ -9,24 +10,45 @@ const ThemeToggle = ({ theme }) => {
   /* Retreive toggle function from context */
   const { toggle } = useThemes()
 
-  const Toggle = styled.button`
-    margin: 1.5rem 1.5rem 0 0;
-    color: ${textColor};
-    background-color: ${backgroundColor};
-    transition: opacity 0.4s;
-    opacity: 0.7;
-    &:hover {
-      opacity: 1;
+  const toggleLightTheme = () => {
+    if (theme.mode === 'dark') {
+      toggle()
     }
+  }
+  const toggleDarkTheme = () => {
+    if (theme.mode === 'light') {
+      toggle()
+    }
+  }
+
+  const Toggle = styled.div`
+    margin: 1.5rem 1.5rem 0 0;
+    color: ${backgroundColor};
+    width: 6.5rem;
+    height: 2.5rem;
+    border: 2px solid ${backgroundColor};
+  `
+  const Button = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1rem;
+    width: 50%;
+    height: 100%;
+    text-align: center;
+    background: ${props => (props.active ? backgroundColor : 'transparent')};
+    color: ${props => (props.active ? textColor : '333')};
   `
 
   return (
-    <div className="absolute lg:fixed top-0 right-0 z-10">
-      <Toggle
-        onClick={toggle}
-        className="text-sm sm:text-base font-semibold rounded-lg px-3 py-2"
-      >
-        {theme.mode === 'light' ? 'Go Dark' : 'Go Light'}
+    <div id="toggle-wrapper" className="absolute lg:fixed top-0 right-0 z-10">
+      <Toggle className="flex justify-center items-center rounded">
+        <Button onClick={toggleLightTheme} active={theme.mode === 'light'}>
+          <FiSun />
+        </Button>
+        <Button onClick={toggleDarkTheme} active={theme.mode === 'dark'}>
+          <FiMoon />
+        </Button>
       </Toggle>
     </div>
   )
