52f2be29805ab648ee63f237c57f2db1a7701b9f
Alberto Zanotti <albizan@hotmail.it>
2019-08-09 19:00:57 +0200

    Removed a Skill

--- src/components/Resume/Skills/Skills.jsx
+++ src/components/Resume/Skills/Skills.jsx
@@ -6,9 +6,8 @@ const Skills = () => {
     <div id="Skills Wrapper">
       <Skill skill="HTML" value="70%" />
       <Skill skill="CSS" value="60%" />
-      <Skill skill="Javascript - Browser" value="60%" />
-      <Skill skill="Javascript - Server" value="70%" />
-      <Skill skill="Typescript" value="70%" />
+      <Skill skill="Javascript" value="60%" />
+      <Skill skill="ES5/Typescript" value="70%" />
       <Skill skill="Reactjs" value="65%" />
       <Skill skill="Nodejs" value="75%" />
     </div>
