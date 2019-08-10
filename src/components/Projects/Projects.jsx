6b06f0045cef772ad8b40c168940f08623c83dcb
Alberto Zanotti <albizan@hotmail.it>
2019-08-10 23:21:44 +0200

    Display mock projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import styled, { withTheme } from 'styled-components'
 
+import Project from './Project'
+
+import { backgroundColor } from '../../themes/projects'
+
 const Projects = () => {
-  return <div />
+  const ProjectsSection = styled.section`
+    background-color: ${backgroundColor};
+  `
+  return (
+    <ProjectsSection className="w-full">
+      <div className="container section-container pb-0">
+        <h3 className="section-title">Some stuff I enjoyed coding</h3>
+      </div>
+      <div className="w-full block md:flex md:justify-start md:items-center md:flex-wrap md:px-8 pb-16">
+        <Project />
+        <Project />
+        <Project />
+      </div>
+    </ProjectsSection>
+  )
 }
 
 export default withTheme(Projects)
