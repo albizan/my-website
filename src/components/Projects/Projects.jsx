17a5e7f2a5f6b79b0cb4c06b2f7beae2f40c51f9
Alberto Zanotti <albizan@hotmail.it>
2019-08-11 21:28:19 +0200

    Adjusted flex behaviour on small screens

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,20 +6,17 @@ import Project from './Project'
 import { backgroundColor } from '../../themes/projects'
 
 const Projects = () => {
-  const ProjectsSection = styled.section`
-    background-color: ${backgroundColor};
-  `
   return (
-    <ProjectsSection className="w-full">
+    <section className="w-full">
       <div className="container section-container pb-0">
         <h3 className="section-title">Some stuff I enjoyed coding</h3>
       </div>
-      <div className="w-full block md:flex md:justify-start md:items-center md:flex-wrap md:px-8 pb-16">
+      <div className="w-full block sm:flex sm:justify-start sm:items-center sm:flex-wrap md:px-8 pb-16">
         <Project />
         <Project />
         <Project />
       </div>
-    </ProjectsSection>
+    </section>
   )
 }
 
