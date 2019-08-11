210e042826edb6cfb1c2d589355ce5935bdc1231
Alberto Zanotti <albizan@hotmail.it>
2019-08-11 21:27:51 +0200

    Define background color

--- src/themes/projects.js
+++ src/themes/projects.js
@@ -1,16 +1,6 @@
 import theme from 'styled-theming'
 
-export const textColor = theme('mode', {
-  light: '#efefef',
-  dark: '#efefef',
-})
-
-/* Background colors */
-export const backgroundColor = theme('mode', {
-  light: '#f7fafc',
-  dark: '#222',
-})
 export const cardBackground = theme('mode', {
   light: '#fff',
-  dark: '#000',
+  dark: '#292929',
 })
