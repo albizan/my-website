a0581948857a90830e8616e55a278d9bddb58143
Alberto Zanotti <albizan@hotmail.it>
2019-08-21 22:25:49 +0200

    Use data from props instead of hard coded one

--- src/components/Projects/Project/Project.jsx
+++ src/components/Projects/Project/Project.jsx
@@ -8,7 +8,7 @@ import {
 } from '../../../themes/base'
 import { cardBackground, pillTextColor } from '../../../themes/projects'
 
-const Project = () => {
+const Project = ({ img, title, description, categories, technologies }) => {
   const CardProject = styled.div`
     background-color: ${cardBackground};
   `
@@ -24,31 +24,50 @@ const Project = () => {
     color: ${pillTextColor};
     background-color: ${accentColor};
   `
+  const Tech = styled.span`
+    color: ${accentColor};
+  `
 
   return (
     <div
       id="project-wrapper"
-      className="w-full sm:w-1/2 md:w-1/3 lg:1/4 px-2 md:px-3 lg:px-4 mb-4"
+      className="w-full md:w-1/2 lg:w-1/3 px-2 md:px-3 lg:px-4 mb-4"
     >
       <CardProject className="rounded-lg overflow-hidden shadow-lg">
         <img
-          src="https://picsum.photos/500/250"
+          src={img}
+          loading="lazy"
           alt="test"
           className="mx-auto w-full object-cover"
         />
         <div className="p-6">
-          <div id="techs">
-            <CardPill className="rounded-full px-2 py-1 text-2xs tracking-wide font-semibold uppercase mr-2">
-              front-end
-            </CardPill>
+          <div id="categories">
+            {categories.map((category, i) => (
+              <CardPill
+                key={i}
+                className="rounded-full px-2 py-1 text-2xs tracking-wide font-semibold uppercase mr-2"
+              >
+                {category}
+              </CardPill>
+            ))}
           </div>
           <CardTitle className="font-semibold tracking-wide truncate mt-3">
-            This very website
+            {title}
           </CardTitle>
           <CardDescription className="text-xs md:text-sm leading-snug">
-            This is my personal website, here I tried exploring some exciting
-            new libraries.
+            {description}
           </CardDescription>
+          <div id="technologies" className="mt-8">
+            <p className="font-semibold">Tech used:</p>
+            {technologies.map((tech, i) => (
+              <Tech
+                key={i}
+                className="px-1 text-base"
+              >
+                {tech}
+              </Tech>
+            ))}
+          </div>
         </div>
       </CardProject>
     </div>
