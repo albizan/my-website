1a50ce9174ee22d0165baa0a69b41fab2b705994
Alberto Zanotti <albizan@hotmail.it>
2019-08-28 23:42:49 +0200

    Define inner structure of ToolCard

--- src/components/Tools/Tool/Tool.jsx
+++ src/components/Tools/Tool/Tool.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import styled, { withTheme } from 'styled-components';
+import React from 'react'
+import styled, { withTheme } from 'styled-components'
 
 const ToolCard = styled.div`
+	user-select: none;
 	display: flex;
 	justify-content: center;
 	align-items: center;
@@ -23,13 +24,71 @@ const ToolCard = styled.div`
 		background-color: rgba(255, 255, 255, 0.1);
 		pointer-events: none;
 	}
-`;
+`
+
+const Button = styled.div`
+	position: absolute;
+	bottom: 1rem;
+	right: 1rem;
+	width: 50px;
+	height: 50px;
+	background: linear-gradient(45deg, #3503ad, #f7308c);
+	border-radius: 50%;
+	cursor: pointer;
+	box-shadow: 1rem 1rem 1rem rgba(0, 0, 0, .5);
+	z-index: 10;
+	transition: .5s;
+	&:hover {
+		box-shadow: none;
+		opacity: .9;
+		bottom: 0rem;
+		right: 0rem;
+		width: 100%;
+		height: 100%;
+		border-radius: 0;
+	}
+`
+const Hint = styled.div`
+	font-size: .7rem;
+	text-transform: uppercase;
+	position: absolute;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	${Button}:hover & {
+		display: none;
+	}
+`
+
+const ToolDescription = styled.div`
+	font-size: .1rem;
+	text-align: center;
+	position: absolute;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	display: flex;
+	flex-direction: column;
+	opacity: 0;
+	justify-content: center;
+	align-items: center;
+	transition: .5s;
+	padding: 1rem;
+	${Button}:hover & {
+		transition-delay: .0s;
+		font-size: 1rem;
+		opacity: 1;
+	}
+`
 
 const Tool = ({ details }) => {
-	const { name, image } = details;
+	const { name, image, description } = details
 
-	let overlayClasses = `absolute inset-0 flex justify-center items-center bg-gray-800`;
-	if (1) overlayClasses += ' opacity-0';
 	return (
 		<div className="w-full md:w-1/2 lg:w-1/3 px-4">
 			<ToolCard>
@@ -37,12 +96,16 @@ const Tool = ({ details }) => {
 					<img src={image} alt={`${name} logo`} className="h-16 w-16" />
 					<h3 className="text-center uppercase mt-6 font-bold tracking-wider">{name}</h3>
 				</div>
-				<div id="card-overlay" className={`${overlayClasses}`}>
-					<p className="text-center text-white">Ciaooooo</p>
-				</div>
+				<Button>
+					<Hint>Why?</Hint>
+					<ToolDescription>
+						<h4 className="mb-4 text-xl font-bold">{name}</h4>
+						{description}
+					</ToolDescription>
+				</Button>
 			</ToolCard>
 		</div>
-	);
-};
+	)
+}
 
-export default withTheme(Tool);
+export default withTheme(Tool)
