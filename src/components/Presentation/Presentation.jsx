dd9536e093a249afe4e2e961c72f4fe9a7003640
Alberto Zanotti <albizan@hotmail.it>
2019-08-29 12:28:19 +0200

    Fix typos

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -1,41 +1,30 @@
 import React from 'react'
 
 const Presentation = () => {
-  return (
-    <section className="container section-container">
-      <h3 className="section-title">Nice to meet you!</h3>
-      <div className="section-text">
-        <p>
-          Welcome to my website, I am glad you visited my personal space, here
-          is where I am proud to show my skills, projects, past experiences and
-          future hopes in the web development field.
-        </p>
-        <p>
-          My name is Alberto Zanotti, I am a 23 year old geek located in
-          beautiful Turin, Italy.
-        </p>
-        <p>
-          I got my bachelor degree in Computer Engineering at the Politecnico di
-          Torino, year 2018.
-        </p>
-        <p>
-          I love new web technologies, both on the front-end and on the
-          back-end. I am currently studying to become a full stack developer
-          with React and Node.
-        </p>
-        <p>
-          As a web developer, officialy I make websites and design restful APIs.
-          But really? Well, I try to build software users appreciate: secure,
-          fast, easy to use and understand with a clean and elegant user
-          interface.
-        </p>
-        <p>
-          I'm persistent, curious and continuously working on improving my
-          capabilities.
-        </p>
-      </div>
-    </section>
-  )
+	return (
+		<section className="container section-container">
+			<h3 className="section-title">Nice to meet you!</h3>
+			<div className="section-text">
+				<p>
+					Welcome to my website, I am glad you visited my personal space, here is where I am proud
+					to show my skills, projects, past experiences and future hopes in the web development
+					field.
+				</p>
+				<p>I am a 23-year-old geek located in beautiful Turin, Italy.</p>
+				<p>I got my bachelor degree in Computer Engineering at the Politecnico di Torino..</p>
+				<p>
+					I love new web technologies, both on the front-end and on the back-end. I am currently
+					studying to become a full stack developer with React and Node.
+				</p>
+				<p>
+					As a web developer, officialy I make websites and design restful APIs. But really? Well, I
+					try to build software users appreciate: secure, fast, easy to use and understand with a
+					clean and elegant user interface.
+				</p>
+				<p>I'm persistent, curious and continuously working on improving my capabilities.</p>
+			</div>
+		</section>
+	)
 }
 
 export default Presentation
