4ecfa55372b8aa6ccfa7ea573f5ab35ef29c1ce9
Alberto Zanotti <albizan@hotmail.it>
2019-08-30 16:23:50 +0200

    Retreive more info from data.json

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,21 +4,32 @@ import Project from './Project'
 import data from '../../data/projects.json'
 
 const Projects = () => {
-  const {projects} = data
-  
-  return (
-    <section className="w-full">
-      <div className="container section-container pb-0">
-        <h3 className="section-title">Some stuff I enjoyed coding</h3>
-      </div>
-      <div className="w-full block sm:flex sm:justify-start sm:items-center sm:flex-wrap pb-16">
-        {projects.map((project) => {
-          const {img, title, description, categories, technologies} = project
-          return <Project key={title} img={img} title={title} description={description} categories={categories} technologies={technologies}/>
-        })}
-      </div>
-    </section>
-  )
+	const { projects } = data
+
+	return (
+		<section className="w-full">
+			<div className="container section-container pb-0">
+				<h3 className="section-title">Some stuff I enjoyed coding</h3>
+			</div>
+			<div className="w-full block sm:flex sm:justify-start sm:items-center sm:flex-wrap pb-16">
+				{projects.map((project) => {
+					const { img, title, description, categories, technologies, link, github } = project
+					return (
+						<Project
+							key={title}
+							img={img}
+							title={title}
+							description={description}
+							categories={categories}
+							technologies={technologies}
+							link={link}
+							github={github}
+						/>
+					)
+				})}
+			</div>
+		</section>
+	)
 }
 
 export default Projects
