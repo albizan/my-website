f98ad0a3e6c67249b61457e9a99500202c09ce50
Alberto Zanotti <albizan@hotmail.it>
2019-09-17 11:56:45 +0200

    Created Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
-import React from 'react';
-import styled, { withTheme } from 'styled-components';
+import React from 'react'
+import styled, { withTheme } from 'styled-components'
 
 /* Custom */
-import ThemeToggle from './components/ThemeToggle';
-import Landing from './components/Landing';
-import Presentation from './components/Presentation';
-import Tools from './components/Tools';
-import Resume from './components/Resume';
-import Projects from './components/Projects';
+import ThemeToggle from './components/ThemeToggle'
+import Landing from './components/Landing'
+import Presentation from './components/Presentation'
+import Resume from './components/Resume'
+import Projects from './components/Projects'
+import Tools from './components/Tools'
+import Footer from './components/Footer'
 
 /* Import Theme variables */
-import { textColor, backgroundColor } from './themes/base';
+import { textColor, backgroundColor } from './themes/base'
 
 function App() {
-	/* Styling App with theme variables */
-	const AppStyleWrapper = styled.section`
-		font-family: 'Poppins', 'sans-serif';
-		background-color: ${backgroundColor};
-		color: ${textColor};
-	`;
+  /* Styling App with theme variables */
+  const AppStyleWrapper = styled.section`
+    font-family: 'Poppins', 'sans-serif';
+    background-color: ${backgroundColor};
+    color: ${textColor};
+  `
 
-	return (
-		<AppStyleWrapper>
-			<ThemeToggle />
-			<Landing />
-			<Presentation />
-			<Resume />
-			<Projects />
-			<Tools />
-		</AppStyleWrapper>
-	);
+  return (
+    <AppStyleWrapper>
+      <ThemeToggle />
+      <Landing />
+      <Presentation />
+      <Resume />
+      <Projects />
+      <Tools />
+      <Footer />
+    </AppStyleWrapper>
+  )
 }
 
 /* Export HOC, use styled-theming to inject theme prop to App component */
-export default withTheme(App);
+export default withTheme(App)
