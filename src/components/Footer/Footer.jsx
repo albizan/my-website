c7574faa62353978e7ff2acf39d7b5f59eae5f50
Alberto Zanotti <albizan@hotmail.it>
2019-09-17 18:22:41 +0200

    Updated Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,31 @@
 import React, { Fragment } from 'react'
+import { FaHeart } from 'react-icons/fa'
+import styled from 'styled-components'
+import { accentColor } from '../../themes/base'
+
+const FooterContent = styled.p`
+  display: block;
+  text-align: center;
+  background-color: ${accentColor};
+  color: white;
+  padding: 2rem;
+  letter-spacing: 2px;
+
+  @media screen {
+  }
+`
 
 const Footer = () => {
-  return <Fragment></Fragment>
+  return (
+    <Fragment>
+      <section className="w-full">
+        <FooterContent>
+          Made with <FaHeart className="inline" /> by Alberto Zanotti with react
+          and tailwind
+        </FooterContent>
+      </section>
+    </Fragment>
+  )
 }
 
 export default Footer
